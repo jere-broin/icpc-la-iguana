41d49fcead9ce13650911ac92e2a17f126e6bbfe
Edu175 <eduardocarranzavelez@gmail.com>
2025-07-21 17:57:08 -0300

    removed recursive, added iterative segment tree

--- data_structures/segment_tree_test2.cpp
+++ data_structures/segment_tree_test2.cpp
@@ -1,16 +1,26 @@
-// CSES Dynamic Range Minimum Queries - AC
-// https://cses.fi/problemset/task/1649/ 
+// CSES Subarray Sum Queries - AC
+// https://cses.fi/problemset/task/1190/ 
 #include <bits/stdc++.h>
 #define fore(i,a,b) for(ll i=a,jet=b;i<jet;++i)
 #define JET ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 using namespace std;
 typedef long long ll;
-
-const ll INF=1e15;
-typedef ll node;
-#define oper min
-#define NEUT INF
-struct STree{ // segment tree for min over long long integers
+ 
+struct node{
+	ll sum,pre,suf,all;
+	node(ll a):sum(a),pre(a),suf(a),all(a){}
+	node(){}
+};
+node oper(node a, node b){ // a left, b right
+	node p;
+	p.sum=max({a.sum,b.sum,a.suf+b.pre});
+	p.pre=max(a.pre,a.all+b.pre);
+	p.suf=max(b.suf,a.suf+b.all);
+	p.all=a.all+b.all;
+	return p;
+}
+node NEUT=node(0);
+struct STree{
 	int n; vector<node>t;
 	STree(int n):n(n),t(2*n+5,NEUT){}
 	void init(vector<node> &a){
@@ -30,23 +40,19 @@ struct STree{ // segment tree for min over long long integers
 	}
 }; // usage: STree rmq(n);rmq.init(x);rmq.upd(i,v);rmq.query(s,e);
 
+ 
 int main(){
 	JET
 	ll n,q; cin>>n>>q;
-	vector<ll> a(n),ini(n);
-	fore(i,0,n)cin>>a[i],ini[i]=a[i];
+	vector<ll> a(n);
+	vector<node> ini(n);
+	fore(i,0,n)cin>>a[i],ini[i]=node(a[i]);
 	STree st(n);
 	st.init(ini);
 	while(q--){
-		ll ty; cin>>ty;
-		if(ty==1){
-			ll k,u; cin>>k>>u; k--;
-			st.upd(k,u);
-		}
-		else {
-			ll l,r; cin>>l>>r; l--;
-			cout<<st.query(l,r)<<"\n";
-		}
+		ll k,v; cin>>k>>v; k--;
+		st.upd(k,v);
+		cout<<max(st.query(0,n).sum,0ll)<<"\n";
 	}
 	return 0;
 }
