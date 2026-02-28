bdd3c30ebc1d0e3b5e5ffbb522db41dd9895d635
Tiziano Arduino Brunelli Della Mea <tiziano.brunelli@mi.unc.edu.ar>
2026-02-28 21:21:36 +0000

    added Iguana´s segment tree, segment tree lazy and centroid. Also minors changes in the tex file and geometry formulas

--- data_structures/segment_tree.cpp
+++ data_structures/segment_tree.cpp
@@ -1,28 +1,23 @@
+// works in all cases, even non-commutative operations (see test2)
+typedef ll node;
 #define oper min
 #define NEUT INF
-struct STree { // segment tree for min over integers
-	vector<int> st;int n;
-	STree(int n): st(4*n+5,NEUT), n(n) {}
-	void init(int k, int s, int e, int *a){
-		if(s+1==e){st[k]=a[s];return;}
-		int m=(s+e)/2;
-		init(2*k,s,m,a);init(2*k+1,m,e,a);
-		st[k]=oper(st[2*k],st[2*k+1]);
+struct STree{ // segment tree for min over long long integers
+	int n; vector<node>t;
+	STree(int n):n(n),t(2*n+5,NEUT){}
+	void init(vector<node> &a){
+		fore(i,0,n)t[n+i]=a[i];
+		for(ll i=n-1;i>0;i--)t[i]=oper(t[2*i],t[2*i+1]);
 	}
-	void upd(int k, int s, int e, int p, int v){
-		if(s+1==e){st[k]=v;return;}
-		int m=(s+e)/2;
-		if(p<m)upd(2*k,s,m,p,v);
-		else upd(2*k+1,m,e,p,v);
-		st[k]=oper(st[2*k],st[2*k+1]);
+	void upd(int p, node v){
+		for(p+=n,t[p]=v;p>1;p>>=1)p|=1,t[p>>1]=oper(t[p^1],t[p]);
 	}
-	int query(int k, int s, int e, int a, int b){
-		if(s>=b||e<=a)return NEUT;
-		if(s>=a&&e<=b)return st[k];
-		int m=(s+e)/2;
-		return oper(query(2*k,s,m,a,b),query(2*k+1,m,e,a,b));
+	node query(int l, int r){
+		node izq=NEUT,der=NEUT;
+		for(l+=n,r+=n;l<r;l>>=1,r>>=1){
+			if(l&1)izq=oper(izq,t[l++]);
+			if(r&1)der=oper(t[--r],der);
+		}
+		return oper(izq,der);
 	}
-	void init(int *a){init(1,0,n,a);}
-	void upd(int p, int v){upd(1,0,n,p,v);}
-	int query(int a, int b){return query(1,0,n,a,b);}
 }; // usage: STree rmq(n);rmq.init(x);rmq.upd(i,v);rmq.query(s,e);
--- data_structures/segment_tree_lazy.cpp
+++ data_structures/segment_tree_lazy.cpp
@@ -1,42 +1,58 @@
-struct STree {//example: sum query & sum range upd
-	vector<int> st,lazy;int n;
-	STree(int n): st(4*n+5,0), lazy(4*n+5,0), n(n) {}
-	void init(int k, int s, int e, int *a){
-		lazy[k]=0;  // lazy neutral element
-		if(s+1==e){st[k]=a[s];return;}
-		int m=(s+e)/2;
-		init(2*k,s,m,a);init(2*k+1,m,e,a);
-		st[k]=st[2*k]+st[2*k+1]; // operation
+// example: range sum with range addition
+typedef ll tn; // segment-tree node type
+typedef ll tl; // lazy type
+#define NEUT 0 // operation neutral
+#define CLEAR 0 // cleared lazy node
+tn oper(tn a, tn b){ // segment tree operation
+	return a+b;
+}
+void acum(tl &a, tl v){ // accumulate lazy
+	a+=v;
+}
+tn calc(int s, int e, tn a, tl v){ // calculate new segment tree value of node with range [s,e), previous value a, and lazy update v
+	a+=(e-s)*v;
+	return a;
+}
+struct STree{
+	vector<tn>st; vector<tl>lazy; int n;
+	STree(int n):st(4*n+5,NEUT),lazy(4*n+5,CLEAR),n(n){}
+	void init(int k, int s, int e, vector<tn>&a){
+		if(e-s==1)st[k]=a[s];
+		else {
+			int m=(s+e)/2;
+			init(2*k,s,m,a); init(2*k+1,m,e,a);
+			st[k]=oper(st[2*k],st[2*k+1]);
+		}
 	}
 	void push(int k, int s, int e){
-		if(!lazy[k])return;//if neutral, nothing to do
-		st[k]+=(e-s)*lazy[k];//upd st according to lazy
-		if(s+1<e){ // propagate to children
-			lazy[2*k]+=lazy[k];
-			lazy[2*k+1]+=lazy[k];
+		if(lazy[k]==CLEAR)return;
+		st[k]=calc(s,e,st[k],lazy[k]);
+		if(e-s!=1){
+			acum(lazy[2*k],lazy[k]);
+			acum(lazy[2*k+1],lazy[k]);
 		}
-		lazy[k]=0; // clear node lazy
+		lazy[k]=CLEAR;
 	}
-	void upd(int k, int s, int e, int a, int b, int v){
+	void upd(int k, int s, int e, int a, int b, tl v){
 		push(k,s,e);
-		if(s>=b||e<=a)return;
-		if(s>=a&&e<=b){
-			lazy[k]+=v; // accumulate lazy
-			push(k,s,e);return;
+		if(e<=a||b<=s)return;
+		if(a<=s&&e<=b){
+			acum(lazy[k],v);
+			push(k,s,e);
+			return;
 		}
 		int m=(s+e)/2;
-		upd(2*k,s,m,a,b,v);upd(2*k+1,m,e,a,b,v);
-		st[k]=st[2*k]+st[2*k+1]; // operation
+		upd(2*k,s,m,a,b,v),upd(2*k+1,m,e,a,b,v);
+		st[k]=oper(st[2*k],st[2*k+1]);
 	}
-	int query(int k, int s, int e, int a, int b){
-		if(s>=b||e<=a)return 0; // operation neutral
+	tn query(int k, int s, int e, int a, int b){
+		if(e<=a||b<=s)return NEUT;
 		push(k,s,e);
-		if(s>=a&&e<=b)return st[k];
+		if(a<=s&&e<=b)return st[k];
 		int m=(s+e)/2;
-		return query(2*k,s,m,a,b)+query(2*k+1,m,e,a,b);// operation
+		return oper(query(2*k,s,m,a,b),query(2*k+1,m,e,a,b));
 	}
-	void init(int *a){init(1,0,n,a);}
-	void upd(int a, int b, int v){upd(1,0,n,a,b,v);}
-	int query(int a, int b){return query(1,0,n,a,b);}
-}; // usage: STree rmq(n);rmq.init(x);
-//rmq.upd(s,e,v);rmq.query(s,e);
+	void upd(int a, int b, tl v){upd(1,0,n,a,b,v);}
+	tn query(int a, int b){return query(1,0,n,a,b);}
+	void init(vector<tn>&a){init(1,0,n,a);}
+};
