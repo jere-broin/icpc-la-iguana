c8824ab9c78a1e6c851d199e6b07013371e03f7b
Tiziano Arduino Brunelli Della Mea <tiziano.brunelli@mi.unc.edu.ar>
2026-03-02 03:47:48 +0000

    minor changes

--- geometry/convex_hull.cpp
+++ geometry/convex_hull.cpp
@@ -1,7 +1,5 @@
-// CCW order
-// Includes collinear points (change sign of EPS in left to exclude)
-vector<pt> chull(vector<pt> p){
-	if(SZ(p)<3) return p;
+vector<pt> chull(vector<pt> p){// CCW order
+	if(SZ(p)<3) return p;//incl collinear
 	vector<pt> r; sort(ALL(p)); int k=0;
 	fore(_,0,2){//first lower hull, then upper hull
 		fore(i,0,SZ(p)){
