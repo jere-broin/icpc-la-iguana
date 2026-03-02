6dcf76c8a80b3c1de9eee47194b5e61ad917514f
Jeremias Broin Luque <jbroin@seenka.com>
2026-03-01 21:55:45 -0300

    erase enter in mec

--- geometry/mec.cpp
+++ geometry/mec.cpp
@@ -5,7 +5,6 @@ pt circumcenter(pt A, pt B, pt C) { // intersection of perpendicular bisectors
     ld a=(B-C).norm2(), b=(C-A).norm2(), c=(A-B).norm2();
     return bary(A, B, C, a*(b+c-a), b*(c+a-b), c*(a+b-c));
 }
-
 random_device rd;
 mt19937 rng(rd());
 pair<pt,ld> mec(vector<pt> ps) {
@@ -25,7 +24,6 @@ pair<pt,ld> mec(vector<pt> ps) {
 	}
 	return {o,r};
 }
-
 --this points not affect mec
 pt centroid(pt A, pt B, pt C) { // geometric center of mass
     return bary(A, B, C, 1, 1, 1);
@@ -33,12 +31,10 @@ pt centroid(pt A, pt B, pt C) { // geometric center of mass
 pt incenter(pt A, pt B, pt C) { // intersection of internal angle bisectors
     return bary(A, B, C, (B-C).norm(), (A-C).norm(), (A-B).norm());
 }
-
 pt orthocenter(pt A, pt B, pt C) { // norm is squared  // intersection of altitudes
     double a = (B - C).norm2(), b = (C - A).norm2(), c = (A - B).norm2();
     return bary(A, B, C, (a+b-c)*(c+a-b), (b+c-a)*(a+b-c), (c+a-b)*(b+c-a));
 }
-
 pt excenter(pt A, pt B, pt C) { // intersection of two external angle bisectors
     double a = (B - C).norm(), b = (A - C).norm(), c = (A - B).norm();
     return bary(A, B, C, -a, b, c);
