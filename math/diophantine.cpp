4fc61703ced5983f3a25f1fd3725220a6c6e1228
Tiziano Arduino Brunelli Della Mea <tiziano.brunelli@mi.unc.edu.ar>
2026-03-02 03:10:12 +0000

    minor changes

--- math/diophantine.cpp
+++ math/diophantine.cpp
@@ -1,11 +1,10 @@
-ii extendedEuclid (ll a, ll b){ //a * x + b * y = gcd(a,b)
+ii extendedEuclid (ll a, ll b){//a*x+b*y=gcd(a,b)
 	if(!b) return {1,0};
 	auto p=extendedEuclid(b,a%b); ll x=p.snd, y=p.fst-(a/b)*x;
 	if(a*x+b*y<0) x=-x, y=-y;
 	return {x,y};
 }
-ii diophantine(ll a,ll b,ll r) {
-	//a*x+b*y=r where r is multiple of gcd(a,b);
+ii diophantine(ll a,ll b,ll r) {// =r inst of gcd
 	ll d=gcd(a,b); a/=d; b/=d; r/=d;
 	auto p = extendedEuclid(a,b); p.fst*=r; p.snd*=r;
 	return p; // solutions: p+t*(-b,a)/d
