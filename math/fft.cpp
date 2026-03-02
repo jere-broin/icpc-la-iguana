45eb917e61986a82af13816d35189e2561722446
Tiziano Arduino Brunelli Della Mea <tiziano.brunelli@mi.unc.edu.ar>
2026-03-02 04:24:34 +0000

    reduce fft

--- math/fft.cpp
+++ math/fft.cpp
@@ -1,40 +1,10 @@
 // MAXN must be power of 2 !!
 // MOD-1 needs to be a multiple of MAXN !!
-// big mod and primitive root for NTT:
-typedef ll tf;
-typedef vector<tf> poly;
-const tf MOD=2305843009255636993,RT=5;
-// FFT
-struct CD {
-	double r,i;
-	CD(double r=0, double i=0):r(r),i(i){}
-	double real()const{return r;}
-	void operator/=(const int c){r/=c, i/=c;}
-};
-CD operator*(const CD& a, const CD& b){
-	return CD(a.r*b.r-a.i*b.i,a.r*b.i+a.i*b.r);}
-CD operator+(const CD& a, const CD& b){return CD(a.r+b.r,a.i+b.i);}
-CD operator-(const CD& a, const CD& b){return CD(a.r-b.r,a.i-b.i);}
-const double pi=acos(-1.0);
-/* NTT: struct CD {
-	tf x; CD(tf x):x(x){} CD(){}
-};
-CD operator*(const CD& a, const CD& b){return CD(mulmod(a.x,b.x));}
-CD operator+(const CD& a, const CD& b){return CD(addmod(a.x,b.x));}
-CD operator-(const CD& a, const CD& b){return CD(submod(a.x,b.x));}
-vector<tf> rts(MAXN+9,-1);
-CD root(int n, bool inv){
-	tf r=rts[n]<0?rts[n]=pm(RT,(MOD-1)/n):rts[n];
-	return CD(inv?pm(r,MOD-2):r);
-}
-*/
-CD cp1[MAXN+9],cp2[MAXN+9];
-int R[MAXN+9];
+CD cp1[MAXN+9],cp2[MAXN+9]; int R[MAXN+9];
 void dft(CD* a, int n, bool inv){
 	fore(i,0,n)if(R[i]<i)swap(a[R[i]],a[i]);
 	for(int m=2;m<=n;m*=2){
-		double z=2*pi/m*(inv?-1:1); CD wi=CD(cos(z),sin(z)); // FFT
-		// CD wi=root(m,inv); // NTT
+		double z=2*pi/m*(inv?-1:1); CD wi=CD(cos(z),sin(z));
 		for(int j=0;j<n;j+=m){
 			CD w(1);
 			for(int k=j,k2=j+m/2;k2<j+m;k++,k2++){
@@ -42,10 +12,7 @@ void dft(CD* a, int n, bool inv){
 			}
 		}
 	}
-	if(inv)fore(i,0,n)a[i]/=n; // FFT
-	//if(inv){ // NTT
-	//	CD z(pm(n,MOD-2)); fore(i,0,n)a[i]=a[i]*z;
-	//}
+	if(inv)fore(i,0,n)a[i]/=n;
 }
 poly multiply(poly& p1, poly& p2){
 	int n=p1.size()+p2.size()+1,m=1,cnt=0;
@@ -57,7 +24,6 @@ poly multiply(poly& p1, poly& p2){
 	dft(cp1,m,false);dft(cp2,m,false);
 	fore(i,0,m)cp1[i]=cp1[i]*cp2[i];
 	dft(cp1,m,true); poly res; n-=2;
-	fore(i,0,n)res.pb((tf)floor(cp1[i].real()+0.5)); // FFT
-	//fore(i,0,n)res.pb(cp1[i].x); // NTT
+	fore(i,0,n)res.pb((tf)floor(cp1[i].real()+0.5));
 	return res;
 }
