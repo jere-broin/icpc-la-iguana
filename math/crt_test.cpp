7ddfd74b6bd656c1afd5054a1c8d5fed6c692376
Tiziano Arduino Brunelli Della Mea <tiziano.brunelli@mi.unc.edu.ar>
2026-03-02 00:59:30 +0000

    minor changes

--- math/crt_test.cpp
+++ math/crt_test.cpp
@@ -26,16 +26,16 @@ pair<ii,ii> diophantine(ll a,ll b,ll r) {
 #define mod(a,m) (((a)%m+m)%m)
 #define inv(a,m) mod(diophantine(a,m,1).fst.fst,m)
 ii sol(ll a, ll x1, ll m){ //requires inv, diophantine
-  ll d=gcd(a,m); if(d<2) return {mod(x1*inv(a,m),m),m};
-  return x1%d ? ii({-1LL,-1LL}) : sol(a/d,x1/d,m/d);
+  ll d=gcd(a,m);if(d<2)return{mod(x1*inv(a,m),m),m};
+  return x1%d?ii({-1LL,-1LL}):sol(a/d,x1/d,m/d);
 }
 ii crt(vv as, vv bs, vv ms){ //returns:(sol,lcm)
     ll x1=0,m1=1;
     fore(i,0,SZ(ms)){
         auto [x2,m2]=sol(as[i],bs[i],ms[i]);
         if(m2<0||(x1-x2)%gcd(m1,m2))return{-1,-1};
-        if(m1==m2)continue;
-        ll k=diophantine(m2,-m1,x1-x2).fst.snd,l=lcm(m1,m2);
+        if(m1==m2)continue;; ll l=lcm(m1,m2);
+        ll k=diophantine(m2,-m1,x1-x2).fst.snd;
         x1=mod((xl)m1*k+x1,l);m1=l;
     }
     return {x1,m1};
