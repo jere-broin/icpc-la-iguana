279384bdf7dddc3cf3564c50615971f8df5fa0ed
Edu175 <eduardocarranzavelez@gmail.com>
2026-06-06 17:19:12 -0300

    scc: adapted for multiple runs, removed compiler warning

--- graphs/scc_test.cpp
+++ graphs/scc_test.cpp
@@ -1,7 +1,5 @@
-
-/*
-This test is over the problem https://cses.fi/problemset/task/1683
-*/
+// CSES Planets and Kingdoms - AC
+// https://cses.fi/problemset/task/1683
 
 #include<bits/stdc++.h>
 #define fore(i,a,b) for(ll i=a,jet=b;i<jet;i++)
@@ -22,19 +20,20 @@ typedef vector<ll> vv;
 const int MAXN = 2e5+100;
 vv g[MAXN], gt[MAXN], ord;  //gt: transposed graph. ord: order for scc  
 ll vis[MAXN], cmp[MAXN], truth[MAXN], qcmp = 0, n; //cmp[x]:scc of x.truth[x]:bool value for 2sat  
-void dfs(ll x, ll t){
-    if(t) cmp[x] = qcmp; vis[x] = 1;
-    for(auto y:t ? gt[x]:g[x])if(!vis[y]) dfs(y,t);
+void dfsk(ll x, ll t){
+	vis[x] = 1; if(t) cmp[x] = qcmp;
+    for(auto y:t ? gt[x]:g[x])if(!vis[y]) dfsk(y,t);
     if(!t) ord.pb(x);
 }
 void scc(){
-    fore(i,0,n) if(!vis[i]) dfs(i,0);
-    reverse(ALL(ord));mset(vis,0);
-    for(auto x:ord)if(!vis[x]) dfs(x,1),qcmp++;
+	qcmp=0; ord.clear(); mset(vis,0);
+    fore(i,0,n) if(!vis[i]) dfsk(i,0);
+    reverse(ALL(ord)); mset(vis,0);
+    for(auto x:ord)if(!vis[x]) dfsk(x,1),qcmp++;
 }
 // 2SAT, first define n = 2 * vars
 ll neg(ll x){return n-x-1;}
-void addor(ll x,ll y){   
+void add_or(ll x, ll y){   
     g[neg(x)].pb(y); g[neg(y)].pb(x);
     gt[x].pb(neg(y)); gt[y].pb(neg(x));
 }
