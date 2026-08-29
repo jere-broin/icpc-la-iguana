d0274ed95b996d5a4e35d93057a1647677568842
Tiziano Arduino Brunelli Della Mea <tiziano.brunelli@mi.unc.edu.ar>
2026-08-29 00:50:59 +0000

    added Marco's convex hull fix for collinears

--- geometry/convex_hull.cpp
+++ geometry/convex_hull.cpp
@@ -1,12 +1,19 @@
-vector<pt> chull(vector<pt> p){// CCW order
-	if(SZ(p)<3) return p;//incl collinear
-	vector<pt> r; sort(ALL(p)); int k=0;
-	fore(_,0,2){//first lower hull, then upper hull
-		fore(i,0,SZ(p)){
-			while(SZ(r)>1+k&&r.back().left(r[SZ(r)-2],p[i]))r.pop_back();
-			r.pb(p[i]);
-		}
-        r.pop_back(),k=SZ(r),reverse(ALL(p));
+// CCW order
+// Includes collinear points (change sign of EPS in left to exclude)
+vector<pt> chull(vector<pt> p){
+	vector<pt> r;
+	sort(p.begin(),p.end()); // first x, then y
+	p.erase(unique(ALL(p)), p.end());
+	fore(i,0,p.size()){ // lower hull
+		while(r.size()>=2&&r.back().left(r[r.size()-2],p[i]))r.pop_back();
+		r.pb(p[i]);
+	}
+	r.pop_back();
+	int k=r.size(),pr=k-1;
+	for(int i=p.size()-1;i>=0;--i){ // upper hull
+		while(r.size()>=k+2&&r.back().left(r[r.size()-2],p[i]))r.pop_back();
+		while(pr>=0&&p[i]<r[pr]) pr--;
+		if(pr<0||!(p[i]==r[pr])) r.pb(p[i]);
 	}
 	return r;
 }
